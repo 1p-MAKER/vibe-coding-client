af0727d8c4df801cfa27cdd1e24ebd36ed945b45
Ippei Nagamine <1pei.nag@gmail.com>
2025-12-27 15:30:33 +0900

    Initial Commit

---
+++ VibeCodingClient/VibeCodingClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCodingClientApp.swift
+//  VibeCodingClient
+//
+//  Created by 1 on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct VibeCodingClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
